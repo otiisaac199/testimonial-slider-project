6114c46bbc0287658d64593b65a45c1e83baec20
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-06 14:21:38 +0100

    chores: cleaned my code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-// import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 import Testimonial from "./components/Testimonial";
 function App() {
