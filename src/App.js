66532bb529af47a87dd8b12b1e7a665f68e12b28
otiisaac199 <otichinazaisaac19@gmail.com>
2022-10-06 13:11:56 +0100

    features: added a state value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return <section className="section">slider project setup</section>;
 }
 
 export default App;
